1be47b0cd576355d09457f1f4f959c544aecdf4f
Inayat61 <ainayat865@gmail.com>
2024-09-18 15:25:18 +0200

    add aws provider + s3 bucket

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -4,12 +4,20 @@ terraform {
       source  = "auth0/auth0"
       version = "1.6.1"
     }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
   }
   backend "s3" {
     bucket         = "auth0-integration-helloworld"
     key            = "terraform/state.tfstate"
     region         = "us-east-1"
   }
+}
+
+provider "aws" {
+  region = "us-east-1"
 }
 
 # terraform-client
