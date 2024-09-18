1ea5bedeb2741aaaad867ebb3ab68d5351918f1a
Inayat61 <ainayat865@gmail.com>
2024-09-18 14:29:21 +0200

    update workflow & enable tenant setting resource only for now until tf apply is fixed

--- terraform/auth0-tenant.tf
+++ terraform/auth0-tenant.tf
@@ -7,65 +7,65 @@ resource "auth0_tenant" "_" {
   }
 }
 
-resource "auth0_connection" "_" {
-  name                 = "user-pool"
-  strategy             = "auth0"
+# resource "auth0_connection" "_" {
+#   name                 = "user-pool"
+#   strategy             = "auth0"
 
-  options {
-    brute_force_protection = true
-    password_policy                = "excellent"
-    password_history {
-      enable = true
-      size   = 3
-    }
+#   options {
+#     brute_force_protection = true
+#     password_policy                = "excellent"
+#     password_history {
+#       enable = true
+#       size   = 3
+#     }
 
-    password_no_personal_info {
-      enable = true
-    }
+#     password_no_personal_info {
+#       enable = true
+#     }
 
-    password_complexity_options {
-      min_length = 8
-    }
-  }
-}
+#     password_complexity_options {
+#       min_length = 8
+#     }
+#   }
+# }
 
-resource "auth0_client" "_" {
-  name                                = "signin-ui-client"
-  app_type                            = "spa"
-  allowed_origins                     = ["https://${var.auth0_domain}", "http://localhost:3000"]
-  allowed_logout_urls                 = ["https://${var.auth0_domain}/v2/logout", "http://localhost:3000"]
-  grant_types = [
-    "authorization_code",
-  ]
-  callbacks = ["https://${var.auth0_domain}", "http://localhost:3000"]
+# resource "auth0_client" "_" {
+#   name                                = "signin-ui-client"
+#   app_type                            = "spa"
+#   allowed_origins                     = ["https://${var.auth0_domain}", "http://localhost:3000"]
+#   allowed_logout_urls                 = ["https://${var.auth0_domain}/v2/logout", "http://localhost:3000"]
+#   grant_types = [
+#     "authorization_code",
+#   ]
+#   callbacks = ["https://${var.auth0_domain}", "http://localhost:3000"]
 
-  jwt_configuration {
-    lifetime_in_seconds = 300
-    secret_encoded      = true
-    alg                 = "RS256"
-  }
-}
+#   jwt_configuration {
+#     lifetime_in_seconds = 300
+#     secret_encoded      = true
+#     alg                 = "RS256"
+#   }
+# }
 
-# signin ui client & user pool connection association
-resource "auth0_connection_client" "_" {
-  connection_id = auth0_connection._.id
-  client_id     = auth0_client._.id
-}
+# # signin ui client & user pool connection association
+# resource "auth0_connection_client" "_" {
+#   connection_id = auth0_connection._.id
+#   client_id     = auth0_client._.id
+# }
 
-resource "auth0_client" "forms_vault" {
-  name                                = "auth0-forms-vault-client"
-  app_type                            = "non_interactive"
-  grant_types = [
-    "client_credentials",
-  ]
-}
+# resource "auth0_client" "forms_vault" {
+#   name                                = "auth0-forms-vault-client"
+#   app_type                            = "non_interactive"
+#   grant_types = [
+#     "client_credentials",
+#   ]
+# }
 
-data "auth0_resource_server" "_" {
-  identifier = "https://${var.auth0_domain}/api/v2/"
-}
+# data "auth0_resource_server" "_" {
+#   identifier = "https://${var.auth0_domain}/api/v2/"
+# }
 
-resource "auth0_client_grant" "forms_vault_client_grant" {
-  client_id = auth0_client.forms_vault.id
-  audience  = data.auth0_resource_server._.identifier
-  scopes    = ["read:users", "update:users", "create:users", "read:users_app_metadata", "update:users_app_metadata", "create:users_app_metadata"]
-}
+# resource "auth0_client_grant" "forms_vault_client_grant" {
+#   client_id = auth0_client.forms_vault.id
+#   audience  = data.auth0_resource_server._.identifier
+#   scopes    = ["read:users", "update:users", "create:users", "read:users_app_metadata", "update:users_app_metadata", "create:users_app_metadata"]
+# }
